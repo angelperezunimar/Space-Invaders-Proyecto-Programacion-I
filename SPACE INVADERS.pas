7dd1e865af265b09ca6979e5fb83f6b29c730ce9
Samuel <smarcano.8764@unimar.edu.ve>
2023-03-24 23:42:46 -0400

    se creo el procedimiento para validar el nombre, apellido y nickname del usuario

--- SPACE INVADERS.pas
+++ SPACE INVADERS.pas
@@ -7,19 +7,46 @@
 
 
 program untitled;
-
+{Empieza la declaracion de variables, cada una tiene una funcion especifica para el juego y seran explicadas en un archivo de texto}
 uses CRT, DOS;
 var
 	scr: integer;
 	fnp: integer;
-	
-	
-	
+//funcion para la validacion del nombre y apellido
+procedure validacion;
+var
+p1,p2,nickname:string;
+c1,c2,codigo1,codigo2,auxiliar1,auxiliar2:integer;
+begin
+c1:=0;
+c2:=0;
+repeat
+	writeln ('introduzca su nombre:');
+	readln(p1);
+	val (p1,c1,codigo1);
+until codigo1>0;
+repeat
+	writeln('introduzca su apellido');
+	readln (p2);
+	val(p2,c2,codigo2);
+until codigo2 > 0;
+
+writeln ('Ingrese su nombre de usuario, puede usar caracteres y numeres:');
+readln (nickname);
+writeln;
+
+readln();
+end;
 	
 
-	
+		
 	
 BEGIN
+ writeln ('Bienvenido a SPACE INVADERS A.A.S');
+ validacion;
+ 
+ 
+ 
 	
 	
 END.
