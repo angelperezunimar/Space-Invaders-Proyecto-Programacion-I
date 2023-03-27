fac839e93f87951618522008fd21e0d1ae6507fe
angelperezunimar <aperez.9625@unimar.edu.ve>
2023-03-26 20:48:28 -0400

    Se corrige la identacion y se refinan ciertos aspectos del programa

--- SPACE INVADERS.pas
+++ SPACE INVADERS.pas
@@ -4,19 +4,20 @@
 
 // Realizado por: Angel Pérez, Abdl Taktak y Samuel Marcano //
 
-program untitled;
-{Empieza la declaracion de variables, cada una tiene una funcion especifica para el juego y seran explicadas en un archivo de texto}
+program SpaceInvaders;
 uses CRT, DOS;
-var
+var {Empieza la declaracion de variables, cada una tiene una funcion especifica para el juego y seran explicadas en un archivo de texto}
 scr,fnp,opciones,dificultades:integer;
 nickname:string;
+
 //funcion para la validacion del nombre y apellido
+
 procedure validacion;
 var
 p1,p2:string;
 c1,c2,codigo1,codigo2:integer;
 const 
-selfiri = 13; {profe me gusta usar constantes ;D}
+selfiri = 13; //{profe me gusta usar constantes ;D} <== Ese fue Samuel profe 
 begin
 c1:=0;
 c2:=0;
@@ -31,8 +32,6 @@ repeat
 	val(p2,c2,codigo2);
 until codigo2 > 0;
 writeln;
-
-
 end;
 
 BEGIN
@@ -81,13 +80,5 @@ BEGIN
 
  end; //end del primer case
 
- 
- 
- 
- 
- 
- 
-	
-	
-END.//end final del cuerpo
 
+END.//end final del cuerpo
